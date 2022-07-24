61dd7bbc51493fbbb16d735f1feea56ed98cef73
MariaKusakina <m.a.kusakina@gmail.com>
2022-07-24 15:07:47 +0300

    проект доработан

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,6 +1,7 @@
 package ru.netology.javacore;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -8,14 +9,55 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private Todos todos;
+    private int port;
+    private String type;
+    private String task;
+
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.todos = todos;
+        this.port = port;
     }
 
+    public TodoServer(String type, String task) {
+        this.type = type;
+        this.task = task;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+
     public void start() throws IOException {
-        System.out.println("Starting server at " + port + "...");
-        //...
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Starting server at port " + port + ". Запустить Client");
+        while (true) {
+            try (Socket socket = serverSocket.accept();
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream());
+            ) {
+                String task = in.readLine();
+                GsonBuilder gsonBuilder = new GsonBuilder();
+                Gson gson = gsonBuilder.create();
+                TodoServer todos1 = gson.fromJson(task, TodoServer.class);
+                if (todos1.getType().equalsIgnoreCase("ADD")) {
+                    todos.addTask(todos1.getTask());
+                } else if (todos1.getType().equalsIgnoreCase("REMOVE")) {
+                    todos.removeTask(todos1.getTask());
+                }
+                out.println(todos.getAllTasks());
+            } catch (IOException e) {
+                System.out.println("Не могу стартовать сервер");
+                e.printStackTrace();
+            }
+        }
     }
 }
+
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,18 +4,24 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+
+    private final List<String> todos = new ArrayList<>();
 
     public void addTask(String task) {
-        //...
+        todos.add(task);
     }
 
     public void removeTask(String task) {
-        //...
+        todos.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+        StringBuilder stringBuilder = new StringBuilder();
+        todos.stream()
+                .sorted();
+        for (String todo : todos ){
+            stringBuilder.append(todo).append(" ");
+        }
+        return stringBuilder.toString();
     }
-
 }
